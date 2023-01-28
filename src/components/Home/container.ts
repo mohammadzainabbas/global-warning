e7751d4308218ecaca8a7305607cfc72eb38e412
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:31:46 +0100

    Improving code reusability

--- src/components/Home/container.ts
+++ src/components/Home/container.ts
@@ -1,2 +1,10 @@
 import { connect } from "react-redux";
 import { Home } from "./component";
+
+const mapStateToProps = (state) => ({
+	// data: state.ACTION_MANAGEMENT.data,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+	// dispatchAction: () => dispatch(ACTION_MANAGEMENT()),
+});
