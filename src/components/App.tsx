90076bf64fa66e570759d68f177682eeb81dc78f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:03:31 +0100

    New feature added

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import Home from "./components/Home";
+import Home from "./Home";
 
 import { fetchDisasters, fetchEmissions } from "../api/api";
 import { DSVParsedArray } from "d3";
