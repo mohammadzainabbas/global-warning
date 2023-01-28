dc9091952e475a7b30684d82133be04d8e2fea34
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:02:20 +0100

    Optimizing performance

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,10 @@
 import * as d3 from 'd3';
 
-const URL = "/data/";
+const URL = "/data";
 
 export const fetchEmissions = async () => {
     // use `` to append "emissions.csv to variable "URL"
-    const data = await d3.csv($`{}`, d3.autoType);
+    const data = await d3.csv(`${URL}/`, d3.autoType);
 
     // use d3.csv to fetch the data and return it
 
