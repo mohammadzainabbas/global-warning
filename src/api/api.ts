356a55b39b0301c3123534cde28c3a056571641f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:14:55 +0100

    Fixing compatibility issues

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import * as d3 from 'd3';
 
-const URL = "/data";
+const URL = "../data";
 
 export const fetchEmissions = async () => {
     return await d3.csv(`${URL}/emissions.csv`, d3.autoType);
