b7dc56a54882dacc49ec6082ca2e0db5ac6e3532
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:03:03 +0100

    Minor update

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,3 +5,7 @@ const URL = "/data";
 export const fetchEmissions = async () => {
     return await d3.csv(`${URL}/emissions.csv`, d3.autoType);
 };
+
+export const fetchDisasters = async () => {
+    return await d3.csv(`${URL}/natural_disasters.csv`, d3.autoType);
+};
