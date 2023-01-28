d7db166e4e0a4b9c616dac23e32d60c193921c28
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 09:58:29 +0100

    Improving code organization

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,5 @@
 import * as d3 from 'd3';
 
 export const fetchCSV = async () => {
-    return await d3.csv('./data/owid-covid-data.csv', d3.autoType);
+    return await d3.csv('/data/.csv', d3.autoType);
 };
