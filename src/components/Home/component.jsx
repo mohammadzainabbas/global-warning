638d503fc77f9279615fff55dc05e8b72edb8314
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 13:46:17 +0100

    Improving code maintainability

--- src/components/Home/component.jsx
+++ src/components/Home/component.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 const Home = ({ data }) => {
 	const {emissions, disasters} = data;
-	
+
 	return (
 		<React.Fragment>
 			<h1>Home</h1>
+			{emissions && (
+				<>
+					<h2>{`Total ${emissions.length} emissions`}</h2>
+				</>
+			 )}
 		</React.Fragment>
 	);
 };
