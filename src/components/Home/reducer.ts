44d7f6fdd5345e8517a05cd0f8458b08fdd175d4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 13:18:29 +0100

    Adding missing sanitization

--- src/components/Home/reducer.ts
+++ src/components/Home/reducer.ts
@@ -2,15 +2,12 @@ import { UPDATE_EMISSIONS, UPDATE_NATURAL_DISASTERS } from "./actions";
 
 export const reducer = (state: any = [], action: any) => {
 	switch (action.type) {
-		case UPDATE_EMISSIONS: 
-		return {
-			...state,
-			forms: {
-				...state.forms,
-				correspondenceInfo: action.payload,
-			}
-		};
-	}
+		case UPDATE_EMISSIONS: {
+			return {
+				...state,
+				...action.payload,
+			};
+		}
 
 		default:
 			return state;
