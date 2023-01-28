761b1073dce82b1ae3e65ce521e927c42c955a71
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 11:50:12 +0100

    Minor changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,8 @@
 import * as d3 from "d3";
 
-const URL = "../../data";
+// const URL = "../../data";
+const URL =
+	"https://raw.githubusercontent.com/mohammadzainabbas/global-warning/main/data";
 
 export const fetchEmissions = async () => {
 	return await d3.csv(`${URL}/emissions.csv`, d3.autoType);
