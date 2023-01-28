6baea0cc2d8d3a54b6c3b0a8e0510686630a9556
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 13:39:41 +0100

    New feature added

--- src/components/Home/component.jsx
+++ src/components/Home/component.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 
 const Home = ({ emissions, disasters }) => {
-	console.group("emissions");
-	console.table(emissions);
-	console.groupEnd();
-
-	console.groupCollapsed("disasters");
-	console.table(disasters);
-	console.groupEnd();
-
 	return (
 		<React.Fragment>
 			<h1>Home</h1>
