27fc136e5764879038857f320e6e67ee80a26fc6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:06:40 +0100

    Code cleanup

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 
 const Home = ({ emissions, disasters }) => {
+	console.group("emissions");
+	console.table(emissions);
+	console.groupEnd();
+
+	console.groupCollapsed("disasters");
+	console.table(disasters);
+	console.groupEnd();
+
 	return (
 		<>
 			<h1>Home</h1>
