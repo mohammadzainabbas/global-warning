43d1bc3e6017b7d37d2a8dd0b21a51826f332b5d
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 13:00:39 +0100

    Adding new functionality

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { homeReducer } from "../components/Home/reducer";
+import { reducer as homeReducer } from "../components/Home/reducer";
 
 const rootReducer = combineReducers({
 	HOME: homeReducer
