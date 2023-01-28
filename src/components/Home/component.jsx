b1d3f2065271ae6c1712878dd2eaa9c93fc1eac0
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:41:13 +0100

    Adding missing sanitization

--- src/components/Home/component.jsx
+++ src/components/Home/component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./styles";
+import styles from "./style";
 import {withStyles} from "@mui/material/styles";
 
 const Home = ({ emissions, disasters }) => {
