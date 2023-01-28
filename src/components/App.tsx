5367c3d6bf569847fc82d9c20f1618cf1475a66d
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:12:07 +0100

    Work in progress

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import "../api/api";
+
 const App = () => {
+	useEffect(() => {}, []);
+
 	return (
 		<React.Fragment>
 			<h1>Hello World</h1>
