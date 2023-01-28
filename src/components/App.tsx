f6370aaa6b35740a266bc37bc9eb46d88cd566a8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:11:25 +0100

    Improving code reusability

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,21 +4,9 @@ import "./App.css";
 
 const App = () => {
 	return (
-		<React.Fragment></React.Fragment>
-
-		// <div className="App">
-		// 	<header className="App-header">
-		// 		<img src={logo} className="App-logo" alt="logo" />
-		// 		<a
-		// 			className="App-link"
-		// 			href="https://reactjs.org"
-		// 			target="_blank"
-		// 			rel="noopener noreferrer"
-		// 		>
-		// 			Learn React
-		// 		</a>
-		// 	</header>
-		// </div>
+		<React.Fragment>
+			<h1>Hello World</h1>
+		</React.Fragment>
 	);
 };
 
