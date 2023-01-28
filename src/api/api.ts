f9da8d4b3c0a7e39f1b01b13f67481a8d63628b4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:02:42 +0100

    Minor update

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,11 +3,5 @@ import * as d3 from 'd3';
 const URL = "/data";
 
 export const fetchEmissions = async () => {
-    // use `` to append "emissions.csv to variable "URL"
-    const data = await d3.csv(`${URL}/`, d3.autoType);
-
-    // use d3.csv to fetch the data and return it
-
-
-    // return await d3.csv(``, d3.autoType);
+    return await d3.csv(`${URL}/emissions.csv`, d3.autoType);
 };
