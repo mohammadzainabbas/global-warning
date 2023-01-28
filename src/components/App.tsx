41acc8f3c56164c49ce668acc67045d83daa72d5
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 11:42:34 +0100

    Code cleanup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,28 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect } from 'react'
+import './App.css'
 
-import { fetchDisasters, fetchEmissions } from "../api/api";
+import { fetchDisasters, fetchEmissions } from '../api/api'
 
 const App = () => {
 	useEffect(() => {
-		(async () => {
-			const disasters = await fetchDisasters();
-			const emissions = await fetchEmissions();
-			console.group("disasters");
-			console.table(disasters);
-			console.groupEnd();
+		;(async () => {
+			const disasters = await fetchDisasters()
+			const emissions = await fetchEmissions()
+			console.group('disasters')
+			console.table(disasters)
+			console.groupEnd()
 
-			console.groupCollapsed("emissions");
-			console.table(emissions);
-			console.groupEnd();
-		})();
-	}, []);
+			console.groupCollapsed('emissions')
+			console.table(emissions)
+			console.groupEnd()
+		})()
+	}, [])
 
 	return (
 		<React.Fragment>
 			<h1>Hello World</h1>
 		</React.Fragment>
-	);
-};
+	)
+}
 
-export default App;
+export default App
