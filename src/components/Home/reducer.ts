caa465ebde9132e2ba01576f67ea2ae74b966e45
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 13:18:08 +0100

    Improving code maintainability

--- src/components/Home/reducer.ts
+++ src/components/Home/reducer.ts
@@ -2,9 +2,15 @@ import { UPDATE_EMISSIONS, UPDATE_NATURAL_DISASTERS } from "./actions";
 
 export const reducer = (state: any = [], action: any) => {
 	switch (action.type) {
-		case UPDATE_EMISSIONS: {
-			...
-		}
+		case UPDATE_EMISSIONS: 
+		return {
+			...state,
+			forms: {
+				...state.forms,
+				correspondenceInfo: action.payload,
+			}
+		};
+	}
 
 		default:
 			return state;
