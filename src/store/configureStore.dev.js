c50b563e6c01d8a355a86e2d9f9daf98584bd242
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:20:13 +0100

    New feature added

---
+++ src/store/configureStore.dev.js
@@ -0,0 +1,27 @@
+import { applyMiddleware, compose, createStore } from "redux";
+import rootReducer from "../reducers/rootReducer";
+import thunk from "redux-thunk";
+import initialState from "./initialState";
+import { createLogger } from "redux-logger";
+// import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+
+// const middleware = [thunk, reduxImmutableStateInvariant()];
+const middleware = [thunk];
+
+middleware.push(createLogger({
+    duration: true,
+    diff: true
+}));
+
+let composedMiddleWare = compose(applyMiddleware(...middleware));
+
+if (!!window.__REDUX_DEVTOOLS_EXTENSION__) {
+    composedMiddleWare = compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
+    );
+}
+
+const store = createStore(rootReducer, initialState, composedMiddleWare);
+
+export default store;
