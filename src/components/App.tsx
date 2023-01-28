647cf9c2240d9baa3ebf1c22946925ff4b932a68
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:00:01 +0100

    Optimizing performance

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,16 +2,19 @@ import React, { useEffect } from "react";
 import "./App.css";
 
 import { fetchDisasters, fetchEmissions } from "../api/api";
+import { DSVParsedArray } from "d3";
 
 const App = () => {
-	const [disasters, setDisasters] = React.useState<DSVParsedArray<object>>([]);
-	const [emissions, setEmissions] = React.useState<DSVParsedArray<object>>([]);
+	const [disasters, setDisasters] = React.useState<DSVParsedArray<object> | []>([]);
+	const [emissions, setEmissions] = React.useState<DSVParsedArray<object> | []>([]);
 
 	useEffect(() => {
 		(async () => {
 			const _disasters = await fetchDisasters();
 			const _emissions = await fetchEmissions();
 
+			setDisasters(_disasters);
+			setEmissions(_emissions);
 			// console.group("disasters");
 			// console.table(disasters);
 			// console.groupEnd();
@@ -20,8 +23,6 @@ const App = () => {
 			// console.table(emissions);
 			// console.groupEnd();
 		})();
-		setDisasters(_disasters);
-		setEmissions(_emissions);
 	}, []);
 
 	return (
