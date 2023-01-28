dd001fa8603dc453983165a86b210b069fe79451
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:07:36 +0100

    Improving code maintainability

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,19 +4,21 @@ import "./App.css";
 
 const App = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<></>
+
+		// <div className="App">
+		// 	<header className="App-header">
+		// 		<img src={logo} className="App-logo" alt="logo" />
+		// 		<a
+		// 			className="App-link"
+		// 			href="https://reactjs.org"
+		// 			target="_blank"
+		// 			rel="noopener noreferrer"
+		// 		>
+		// 			Learn React
+		// 		</a>
+		// 	</header>
+		// </div>
 	);
 };
 
