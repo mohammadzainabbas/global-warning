8826f595b71791f833e645904724c7597c6cc10d
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 09:51:52 +0100

    Work in progress

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,6 +2,5 @@
 import { csv } from 'd3-fetch';
 
 export const fetchCSV = async () => {
-  const data = await csv('https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv');
-  return data;
+    return await csv('./data/owid-covid-data.csv');
 }
