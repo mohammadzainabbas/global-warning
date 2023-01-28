8e227612797f84b1178e77af1c1209185a6ea8f8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 09:57:26 +0100

    Improving code reusability

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,7 @@
 // write a function to fetch the data from the csv file
-
+import * as d3 from 'd3';
 
 
 export const fetchCSV = async () => {
-    return await csv('./data/owid-covid-data.csv', (d) => {
-
+    return await d3.csv('./data/owid-covid-data.csv', d3.autoType);
 };
