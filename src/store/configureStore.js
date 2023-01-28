a10b5930156a7ba75898b722ef92cf9f84d7a999
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:19:52 +0100

    Improving code structure

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,7 @@
 let store = null;
 
 if (process.env.NODE_ENV === "production") {
-    // module.exports = require("./configureStore.prod");
-    store = pro;
+    
 } else {
     // module.exports = require("./configureStore.dev");
     store = dev;
