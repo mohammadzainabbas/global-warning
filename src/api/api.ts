7e239737c989aab38a1ce383f693301426e51429
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:01:18 +0100

    Improving code organization

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,5 +3,11 @@ import * as d3 from 'd3';
 const URL = "/data/";
 
 export const fetchEmissions = async () => {
-    return await d3.csv(``, d3.autoType);
+    // use `` to append "emissions.csv to variable "URL"
+    const data = await d3.csv(``, d3.autoType);
+        
+    // use d3.csv to fetch the data and return it
+
+
+    // return await d3.csv(``, d3.autoType);
 };
