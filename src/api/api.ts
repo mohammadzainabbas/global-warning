5f58bc323695198676e8220ab0c7e63437850943
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 09:57:05 +0100

    Minor changes

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 // write a function to fetch the data from the csv file
-import { csv } from 'd3-fetch';
+
+
 
 export const fetchCSV = async () => {
     return await csv('./data/owid-covid-data.csv', (d) => {
-        
+
 };
