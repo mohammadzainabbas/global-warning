ceeb2bc35cf76bf34766ddff638a59eda62afa7f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:21:37 +0100

    Minor update

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, compose, createStore } from "redux";
+import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
 import rootReducer from "../reducers/rootReducer";
 import thunk from "redux-thunk";
 import initialState from "./initialState";
