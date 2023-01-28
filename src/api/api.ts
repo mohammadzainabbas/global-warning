e2319917b842a54a050b2d2b702c38a721cc661b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 09:57:47 +0100

    Fixing compatibility issues

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,4 @@
-// write a function to fetch the data from the csv file
 import * as d3 from 'd3';
-
 
 export const fetchCSV = async () => {
     return await d3.csv('./data/owid-covid-data.csv', d3.autoType);
