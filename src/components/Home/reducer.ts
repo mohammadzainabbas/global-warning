4abec58d6ee9f1111c8486bd607ae592c9d8b5d2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 13:17:26 +0100

    Updating README.md

--- src/components/Home/reducer.ts
+++ src/components/Home/reducer.ts
@@ -1,5 +1,9 @@
+import { UPDATE_EMISSIONS, UPDATE_NATURAL_DISASTERS } from "./actions";
+
 export const reducer = (state: any = [], action: any) => {
 	switch (action.type) {
+		case "GET_DATA":
+
 		default:
 			return state;
 	}
