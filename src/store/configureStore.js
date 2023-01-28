b85ff21fb2be94ac486177e83cf82b98f7602dbe
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:23:01 +0100

    Improving code organization

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,10 @@
-let store = null;
+import dev from "./configureStore.dev";
+import pro from "./configureStore.prod";
 
+let store = null;
 if (process.env.NODE_ENV === "production") {
-    
+    // module.exports = require("./configureStore.prod");
+    store = pro;
 } else {
     // module.exports = require("./configureStore.dev");
     store = dev;
