fefafc75931658e6005f6e6bf3e07ae109db5b1e
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:59:57 +0100

    Improving code quality

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./store/configureStore";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-
-import { Provider } from "react-redux";
-import store from "./store/configureStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
