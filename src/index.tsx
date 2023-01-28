2c475116369df3a333e0a279bedbbe2b983100ce
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 10:04:48 +0100

    Adding missing documentation

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from '../components/';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 import '@fontsource/roboto/300.css';
