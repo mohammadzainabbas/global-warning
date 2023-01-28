f4d7478e3d4a8ccb98538e5b9f815829ea936b4b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-28 12:35:38 +0100

    Minor update

--- src/components/Home/component.jsx
+++ src/components/Home/component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {styles} from "./styles";
 import {withStyles} from "@material-ui/core/styles";
 
-
 const Home = ({ emissions, disasters }) => {
 	console.group("emissions");
 	console.table(emissions);
