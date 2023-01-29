81243c0871dbfe857888ef4ccf4fb44d754ac328
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 21:34:24 +0100

    Optimizing performance

--- src/components/router/routes.jsx
+++ src/components/router/routes.jsx
@@ -1,14 +1,14 @@
 import { Navigate, useRoutes } from 'react-router-dom';
 // layouts
-import DashboardLayout from './layouts/dashboard';
-import SimpleLayout from './layouts/simple';
+import DashboardLayout from '../layouts/dashboard';
+import SimpleLayout from '../layouts/simple';
 //
-import BlogPage from './pages/BlogPage';
-import UserPage from './pages/UserPage';
-import LoginPage from './pages/LoginPage';
-import Page404 from './pages/Page404';
-import ProductsPage from './pages/ProductsPage';
-import DashboardAppPage from './pages/DashboardAppPage';
+import BlogPage from '../pages/BlogPage';
+import UserPage from '../pages/UserPage';
+import LoginPage from '../pages/LoginPage';
+import Page404 from '../pages/Page404';
+import ProductsPage from '../pages/ProductsPage';
+import DashboardAppPage from '../pages/DashboardAppPage';
 
 // ----------------------------------------------------------------------
 
