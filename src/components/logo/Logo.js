0516750ce293cc7350f41d471e60e47177ad7e34
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:05:07 +0100

    Improving usability

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -12,16 +12,16 @@ const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
 
   const logo = (
     <>
-    <Box
-      component="img"
-      src="https://raw.githubusercontent.com/mohammadzainabbas/global-warning/main/public/favicon/android-chrome-512x512.png"
-      sx={{ width: 40, height: 40, cursor: 'pointer', ...sx }}
-    />
-  <Box sx={{ my: 1.5, px: 2.5 }}>
-<Typography variant="subtitle2" noWrap>
-  {`Global Warning`}
-</Typography>
-</Box>
+      <Box
+        component="img"
+        src="https://raw.githubusercontent.com/mohammadzainabbas/global-warning/main/public/favicon/android-chrome-512x512.png"
+        sx={{ width: 40, height: 40, cursor: 'pointer', ...sx }}
+      />
+      <Box sx={{ my: 1.5, px: 2.5 }}>
+        <Typography variant="subtitle2" noWrap>
+          {`Global Warning`}
+        </Typography>
+    </Box>
     </>
   );
 
