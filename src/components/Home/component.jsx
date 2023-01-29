777dbb5ff99b01ab8f0ed4a5b74923f1ad10f150
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 21:12:31 +0100

    New feature added

--- src/components/Home/component.jsx
+++ src/components/Home/component.jsx
@@ -2,16 +2,10 @@ import React from "react";
 
 import { useTheme } from '@mui/material/styles';
 import { Grid, Container, Typography } from '@mui/material';
-// components
-import Iconify from '../components/iconify';
-// sections
 import {
   AppTasks,
-  AppNewsUpdate,
-  AppOrderTimeline,
   AppCurrentVisits,
   AppWebsiteVisits,
-  AppTrafficBySite,
   AppWidgetSummary,
   AppCurrentSubject,
   AppConversionRates,
