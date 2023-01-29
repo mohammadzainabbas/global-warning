102a493283b652bb532f07191f002b927b920141
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 00:04:55 +0100

    Improving usability

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,3 +3,8 @@ export const HOME = "/";
 export const GITHUB_REPO_LINK = "https://github.com/mohammadzainabbas/global-warning/";
 export const ABOUT = "/about";
 export const NOT_FOUND = "/404";
+
+// Account details
+export const ACCOUNT = {
+    name: "Mohammad Zain Abbas",
+    email:
