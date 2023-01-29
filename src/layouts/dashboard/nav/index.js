fe9a36b7772630278e9bc56bcbbb66b8185ff258
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 00:08:45 +0100

    Optimizing performance

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -54,11 +54,9 @@ export default function Nav({ openNav, onCloseNav }) {
         '& .simplebar-content': { height: 1, display: 'flex', flexDirection: 'column' },
       }}
     >
-      <Box sx={{ px: 2.5, py: 3, display: 'inline-flex' }}>
+      <Box sx={{ px: 2.5, py: 2, display: 'inline-flex' }}>
         <Logo />
       </Box>
-
-      <Divider sx={{ borderStyle: 'dashed' }} />
 
       <Box sx={{ mb: 5, mx: 2.5 }}>
         <Link underline="none">
