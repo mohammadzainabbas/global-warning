ca593870eee55b133993f5cbdecc4ff0e360c2fa
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 21:41:00 +0100

    Improving code maintainability

--- src/components/router/routes.jsx
+++ src/components/router/routes.jsx
@@ -20,9 +20,6 @@ export default function Router() {
       children: [
         { element: <Navigate to="/dashboard/app" />, index: true },
         { path: 'app', element: <DashboardAppPage /> },
-        { path: 'user', element: <UserPage /> },
-        { path: 'products', element: <ProductsPage /> },
-        { path: 'blog', element: <BlogPage /> },
       ],
     },
     {
