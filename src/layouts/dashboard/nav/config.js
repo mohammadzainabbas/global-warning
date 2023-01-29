9ace273af079d2f312b274351479aee4c3f8cf61
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:50:38 +0100

    Adding new functionality

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -13,27 +13,7 @@ const navConfig = [
     icon: icon('ic_analytics'),
   },
   {
-    title: 'user',
-    path: '/dashboard/user',
-    icon: icon('ic_user'),
-  },
-  {
-    title: 'product',
-    path: '/dashboard/products',
-    icon: icon('ic_cart'),
-  },
-  {
-    title: 'blog',
-    path: '/dashboard/blog',
-    icon: icon('ic_blog'),
-  },
-  {
-    title: 'login',
-    path: '/login',
-    icon: icon('ic_lock'),
-  },
-  {
-    title: 'Not found',
+    title: 'View on Github',
     path: '/404',
     icon: icon('ic_disabled'),
   },
