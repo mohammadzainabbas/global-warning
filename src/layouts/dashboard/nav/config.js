95ffa4587614bbe96788dad47c18f7a338c7980b
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:54:50 +0100

    Adding comments for better understanding

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -1,6 +1,8 @@
 // component
 import SvgColor from '../../../components/svg-color';
 import { HOME, GITHUB_REPO_LINK } from '../../../common/constants';
+import HomeIcon from '@mui/icons-material/Home';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 // ----------------------------------------------------------------------
 
