088f09f94eb1d95d4bd740f04d9478567ce24e56
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:02:58 +0100

    Adding new functionality

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -11,11 +11,14 @@ const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
   const theme = useTheme();
 
   const logo = (
+    <>
     <Box
       component="img"
       src="https://raw.githubusercontent.com/mohammadzainabbas/global-warning/main/public/favicon/android-chrome-512x512.png"
       sx={{ width: 40, height: 40, cursor: 'pointer', ...sx }}
     />
+    {`Global Warning`}
+    </>
   );
 
   if (disabledLink) {
