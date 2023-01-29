2b328895aba456b93850264c7ac9bb3998b0c4ff
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 00:20:17 +0100

    Improving code reusability

--- src/layouts/dashboard/header/index.js
+++ src/layouts/dashboard/header/index.js
@@ -14,7 +14,7 @@ import NotificationsPopover from './NotificationsPopover';
 
 // ----------------------------------------------------------------------
 
-const NAV_WIDTH = 280;
+const NAV_WIDTH = 0;
 
 const HEADER_MOBILE = 64;
 
