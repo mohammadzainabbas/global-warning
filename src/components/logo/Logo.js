8564b2cb3d6ad67980241f0cb536e043d59b8338
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:01:13 +0100

    Removing unused code

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -21,7 +21,7 @@ const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
   const logo = (
     <Box
       component="img"
-      src="https://raw.githubusercontent.com/mohammadzainabbas/global-warning/main/public/favicon/favicon-32x32.png"
+      src="https://raw.githubusercontent.com/mohammadzainabbas/global-warning/main/public/favicon/android-chrome-512x512.png"
       sx={{ width: 40, height: 40, cursor: 'pointer', ...sx }}
     />
   );
