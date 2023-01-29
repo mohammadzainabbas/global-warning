e8d718915f3ebfb831e8705242a5ce619a9fc9cd
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 22:17:29 +0100

    Improving code organization

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -21,7 +21,7 @@ const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
   const logo = (
     <Box
       component="img"
-      src="/logo/logo_single.svg"
+      src="/assets/climate-change-icon.png"
       sx={{ width: 40, height: 40, cursor: 'pointer', ...sx }}
     />
   );
