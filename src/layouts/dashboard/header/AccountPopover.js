4b394c84944106eace8fbf4f23dd9c01f551b791
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:22:48 +0100

    New feature added

--- src/layouts/dashboard/header/AccountPopover.js
+++ src/layouts/dashboard/header/AccountPopover.js
@@ -72,7 +72,7 @@ export default function AccountPopover() {
             p: 0,
             mt: 1.5,
             ml: 0.75,
-            width: 180,
+            width: 280,
             '& .MuiMenuItem-root': {
               typography: 'body2',
               borderRadius: 0.75,
