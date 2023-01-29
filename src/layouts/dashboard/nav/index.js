a1314587abc49fabe64aa6e7189de3f1705adf24
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:40:15 +0100

    Fixing bugs

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -80,19 +80,6 @@ export default function Nav({ openNav, onCloseNav }) {
       </Box>
 
       <NavSection data={navConfig} />
-
-      <Box sx={{ flexGrow: 1 }} />
-
-      <Box sx={{ px: 2.5, pb: 3, mt: 10 }}>
-        <Stack alignItems="center" spacing={3} sx={{ pt: 5, borderRadius: 2, position: 'relative' }}>
-          <Box
-            component="img"
-            src="/assets/illustrations/illustration_avatar.png"
-            sx={{ width: 100, position: 'absolute', top: -50 }}
-          />
-
-        </Stack>
-      </Box>
     </Scrollbar>
   );
 
