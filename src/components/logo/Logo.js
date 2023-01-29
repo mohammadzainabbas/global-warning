b9e0a85b1657e71a1015df1bab383381f0d369af
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:00:32 +0100

    Minor update

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -21,7 +21,7 @@ const Logo = forwardRef(({ disabledLink = false, sx, ...other }, ref) => {
   const logo = (
     <Box
       component="img"
-      src="../../../assets/climate-change-icon.png"
+      src="https://raw.githubusercontent.com/mohammadzainabbas/global-warning/main/public/favicon/favicon-32x32.png"
       sx={{ width: 40, height: 40, cursor: 'pointer', ...sx }}
     />
   );
