6da85adcc56a6920d52439cdb1874fd69ea196bd
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 00:29:19 +0100

    Adding new functionality

--- src/layouts/dashboard/DashboardLayout.js
+++ src/layouts/dashboard/DashboardLayout.js
@@ -49,7 +49,9 @@ export default function DashboardLayout() {
               color: 'text.primary',
               display: { lg: 'none' },
             }}
-          />
+          >
+            <MenuIcon />
+          </IconButton>
         </>
       )}
 
