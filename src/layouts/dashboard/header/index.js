8a130c6a4c63d6bb7e3af1fb3a648abc0204aad7
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:22:07 +0100

    Removing unused code

--- src/layouts/dashboard/header/index.js
+++ src/layouts/dashboard/header/index.js
@@ -69,7 +69,7 @@ export default function Header({ onOpenNav }) {
           }}
         >
           {/* <LanguagePopover /> */}
-          <NotificationsPopover />
+          {/* <NotificationsPopover /> */}
           <AccountPopover />
         </Stack>
       </StyledToolbar>
