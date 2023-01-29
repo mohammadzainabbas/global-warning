4f011c00a298d2863b4aaf56f672f190e3f830ff
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 22:14:20 +0100

    Fixing compatibility issues

--- src/components/router/routes.jsx
+++ src/components/router/routes.jsx
@@ -32,7 +32,7 @@ export default function Router() {
     {
       element: <SimpleLayout />,
       children: [
-        { element: <Navigate to="/dashboard/app" />, index: true },
+        // { element: <Navigate to="/dashboard/app" />, index: true },
         { path: '404', element: <Page404 /> },
         { path: '*', element: <Navigate to="/404" /> },
       ],
