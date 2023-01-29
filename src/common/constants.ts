0266d7bbb25f8c8672c87da8f5dcf79dcbb01f9d
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-29 23:51:20 +0100

    Improving code structure

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
 // Route Links
 export const HOME = "/";
-export const GITHUB_REPO_LINK = "
+export const GITHUB_REPO_LINK = "https://github.com/mohammadzainabbas/global-warning/";
 export const ABOUT = "/about";
 export const NOT_FOUND = "/404";
