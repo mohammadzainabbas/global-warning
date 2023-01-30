a34360c2eb25c7544a977879a24687107d4447d4
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 03:16:51 +0100

    Minor changes

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -74,6 +74,24 @@ const Home = (props) => {
 	}, [yearRange, totalDisasters]);
 
 	const onSliderChange = (event, newValue) => {
+
+		if (!Array.isArray(newValue)) {
+			return;
+		}
+
+		if (newValue[1] - newValue[0] < minDistance) {
+			if (activeThumb === 0) {
+				const clamped = Math.min(newValue[0], 100 - minDistance);
+				setValue2([clamped, clamped + minDistance]);
+			} else {
+				const clamped = Math.max(newValue[1], minDistance);
+				setValue2([clamped - minDistance, clamped]);
+			}
+		} else {
+			setValue2(newValue as number[]);
+		}
+
+
 		setYearRange(newValue);
 	};
 
