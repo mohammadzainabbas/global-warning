156f787fab70c1180e21ba37c54b6ea9c43b0411
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:39:18 +0100

    Improving code reusability

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,7 +1,7 @@
 // Route Links
 export const HOME = "/";
-export const TASKS = "/";
 export const GITHUB_REPO_LINK = "https://github.com/mohammadzainabbas/global-warning/";
+export const TASKS = "/tasks";
 export const ABOUT = "/about";
 export const NOT_FOUND = "/404";
 
