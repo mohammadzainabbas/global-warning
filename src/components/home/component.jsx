3200c77b36c2ee4203b296834ddce6dae51aef1a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 04:22:56 +0100

    Improving code structure

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -162,150 +162,151 @@ const Home = (props) => {
 			type,
 			count: disasters.filter((disaster) => disaster.disaster_type === type).length,
 		}
-		debugger
+	});
+	debugger
 
-		return (
-			<React.Fragment>
-				<Helmet>
-					<title>{`Home | Global Warning`}</title>
-				</Helmet>
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Home | Global Warning`}</title>
+			</Helmet>
 
-				{loading ? <Loading /> :
-					<React.Fragment>
-						<Container maxWidth="xl">
-							<Typography variant="h4" sx={{ mb: 5 }}>
-								{years.length ? `Showing stats from ${yearRange[0]} to ${yearRange[1]}` : `Hi, Welcome back`}
-							</Typography>
+			{loading ? <Loading /> :
+				<React.Fragment>
+					<Container maxWidth="xl">
+						<Typography variant="h4" sx={{ mb: 5 }}>
+							{years.length ? `Showing stats from ${yearRange[0]} to ${yearRange[1]}` : `Hi, Welcome back`}
+						</Typography>
 
-							<Grid container spacing={3}>
-								<Grid item xs={12} sm={6} md={3}>
-									<AppWidgetSummary title={`Total recorded deaths`} total={total_deaths} color={`error`} icon={'mdi:emoticon-dead'} />
-								</Grid>
-
-								<Grid item xs={12} sm={6} md={3}>
-									<AppWidgetSummary title={`Total affected`} total={total_affected} color={`warning`} icon={'material-symbols:personal-injury'} />
-								</Grid>
-
-								<Grid item xs={12} sm={6} md={3}>
-									<AppWidgetSummary title={`Total natural disasters`} total={total_disasters} color={`secondary`} icon={'mdi:home-climate-outline'} />
-								</Grid>
-
-								<Grid item xs={12} sm={6} md={3}>
-									<AppWidgetSummary title={`Countries affected`} total={affected_countries} color={`info`} icon={'ph:globe-hemisphere-west-fill'} />
-								</Grid>
-
-								<Grid item xs={12}>
-									<Box sx={{ height: '100%' }}>
-										<Slider
-											getAriaLabel={() => 'Years'}
-											value={yearRange}
-											onChange={onSliderChange}
-											valueLabelDisplay={`auto`}
-											getAriaValueText={(value) => `Year: ${value}`}
-											valueLabelFormat={(value) => `Year: ${value}`}
-											disableSwap
-											marks={marks}
-											min={min(years)}
-											max={max(years)}
-										/>
-									</Box>
-								</Grid>
-
-								<Grid item xs={12} md={6} lg={8}>
-									<AppWebsiteVisits
-										title={`Frequency of disasters`}
-										subheader={`(+43%) than last year`}
-										chartLabels={[
-											'01/01/2003',
-											'02/01/2003',
-											'03/01/2003',
-											'04/01/2003',
-											'05/01/2003',
-											'06/01/2003',
-											'07/01/2003',
-											'08/01/2003',
-											'09/01/2003',
-											'10/01/2003',
-											'11/01/2003',
-										]}
-										chartData={[
-											{
-												name: 'Team A',
-												type: 'column',
-												fill: 'solid',
-												data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-											},
-											{
-												name: 'Team B',
-												type: 'area',
-												fill: 'gradient',
-												data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-											},
-											{
-												name: 'Team C',
-												type: 'line',
-												fill: 'solid',
-												data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-											},
-										]}
-									/>
-								</Grid>
-
-								<Grid item xs={12} md={6} lg={4}>
-									<AppCurrentVisits
-										title="Current Visits"
-										chartData={[
-											{ label: 'America', value: 4344 },
-											{ label: 'Asia', value: 5435 },
-											{ label: 'Europe', value: 1443 },
-											{ label: 'Africa', value: 4443 },
-										]}
-										chartColors={[
-											theme.palette.primary.main,
-											theme.palette.info.main,
-											theme.palette.warning.main,
-											theme.palette.error.main,
-										]}
-									/>
-								</Grid>
-
-								<Grid item xs={12} md={6} lg={8}>
-									<AppConversionRates
-										title="Conversion Rates"
-										subheader="(+43%) than last year"
-										chartData={[
-											{ label: 'Italy', value: 400 },
-											{ label: 'Japan', value: 430 },
-											{ label: 'China', value: 448 },
-											{ label: 'Canada', value: 470 },
-											{ label: 'France', value: 540 },
-											{ label: 'Germany', value: 580 },
-											{ label: 'South Korea', value: 690 },
-											{ label: 'Netherlands', value: 1100 },
-											{ label: 'United States', value: 1200 },
-											{ label: 'United Kingdom', value: 1380 },
-										]}
-									/>
-								</Grid>
-
-								<Grid item xs={12} md={6} lg={4}>
-									<AppCurrentSubject
-										title="Current Subject"
-										chartLabels={['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math']}
-										chartData={[
-											{ name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
-											{ name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
-											{ name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
-										]}
-										chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
-									/>
-								</Grid>
+						<Grid container spacing={3}>
+							<Grid item xs={12} sm={6} md={3}>
+								<AppWidgetSummary title={`Total recorded deaths`} total={total_deaths} color={`error`} icon={'mdi:emoticon-dead'} />
 							</Grid>
-						</Container>
-					</React.Fragment>
-				}
-			</React.Fragment>
-		);
-	};
 
-	export default Home;
+							<Grid item xs={12} sm={6} md={3}>
+								<AppWidgetSummary title={`Total affected`} total={total_affected} color={`warning`} icon={'material-symbols:personal-injury'} />
+							</Grid>
+
+							<Grid item xs={12} sm={6} md={3}>
+								<AppWidgetSummary title={`Total natural disasters`} total={total_disasters} color={`secondary`} icon={'mdi:home-climate-outline'} />
+							</Grid>
+
+							<Grid item xs={12} sm={6} md={3}>
+								<AppWidgetSummary title={`Countries affected`} total={affected_countries} color={`info`} icon={'ph:globe-hemisphere-west-fill'} />
+							</Grid>
+
+							<Grid item xs={12}>
+								<Box sx={{ height: '100%' }}>
+									<Slider
+										getAriaLabel={() => 'Years'}
+										value={yearRange}
+										onChange={onSliderChange}
+										valueLabelDisplay={`auto`}
+										getAriaValueText={(value) => `Year: ${value}`}
+										valueLabelFormat={(value) => `Year: ${value}`}
+										disableSwap
+										marks={marks}
+										min={min(years)}
+										max={max(years)}
+									/>
+								</Box>
+							</Grid>
+
+							<Grid item xs={12} md={6} lg={8}>
+								<AppWebsiteVisits
+									title={`Frequency of disasters`}
+									subheader={`(+43%) than last year`}
+									chartLabels={[
+										'01/01/2003',
+										'02/01/2003',
+										'03/01/2003',
+										'04/01/2003',
+										'05/01/2003',
+										'06/01/2003',
+										'07/01/2003',
+										'08/01/2003',
+										'09/01/2003',
+										'10/01/2003',
+										'11/01/2003',
+									]}
+									chartData={[
+										{
+											name: 'Team A',
+											type: 'column',
+											fill: 'solid',
+											data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+										},
+										{
+											name: 'Team B',
+											type: 'area',
+											fill: 'gradient',
+											data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+										},
+										{
+											name: 'Team C',
+											type: 'line',
+											fill: 'solid',
+											data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+										},
+									]}
+								/>
+							</Grid>
+
+							<Grid item xs={12} md={6} lg={4}>
+								<AppCurrentVisits
+									title="Current Visits"
+									chartData={[
+										{ label: 'America', value: 4344 },
+										{ label: 'Asia', value: 5435 },
+										{ label: 'Europe', value: 1443 },
+										{ label: 'Africa', value: 4443 },
+									]}
+									chartColors={[
+										theme.palette.primary.main,
+										theme.palette.info.main,
+										theme.palette.warning.main,
+										theme.palette.error.main,
+									]}
+								/>
+							</Grid>
+
+							<Grid item xs={12} md={6} lg={8}>
+								<AppConversionRates
+									title="Conversion Rates"
+									subheader="(+43%) than last year"
+									chartData={[
+										{ label: 'Italy', value: 400 },
+										{ label: 'Japan', value: 430 },
+										{ label: 'China', value: 448 },
+										{ label: 'Canada', value: 470 },
+										{ label: 'France', value: 540 },
+										{ label: 'Germany', value: 580 },
+										{ label: 'South Korea', value: 690 },
+										{ label: 'Netherlands', value: 1100 },
+										{ label: 'United States', value: 1200 },
+										{ label: 'United Kingdom', value: 1380 },
+									]}
+								/>
+							</Grid>
+
+							<Grid item xs={12} md={6} lg={4}>
+								<AppCurrentSubject
+									title="Current Subject"
+									chartLabels={['English', 'History', 'Physics', 'Geography', 'Chinese', 'Math']}
+									chartData={[
+										{ name: 'Series 1', data: [80, 50, 30, 40, 100, 20] },
+										{ name: 'Series 2', data: [20, 30, 40, 80, 20, 80] },
+										{ name: 'Series 3', data: [44, 76, 78, 13, 43, 10] },
+									]}
+									chartColors={[...Array(6)].map(() => theme.palette.text.secondary)}
+								/>
+							</Grid>
+						</Grid>
+					</Container>
+				</React.Fragment>
+			}
+		</React.Fragment>
+	);
+};
+
+export default Home;
