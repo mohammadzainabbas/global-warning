ac219f9966c5ea9a8e981436b8a4fbed7337c006
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:05:22 +0100

    Adding missing documentation

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -35,7 +35,7 @@ export default function DashboardAppPage() {
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'mdi:death'} />
+            <AppWidgetSummary title="Weekly Sales" total={714000} icon={'fa-solid:book-dead'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
