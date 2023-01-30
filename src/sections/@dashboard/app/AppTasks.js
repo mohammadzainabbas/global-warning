c567734007f4fe226624fbea5ce8a0adea69ae70
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:43:30 +0100

    Adding missing validation

--- src/sections/@dashboard/app/AppTasks.js
+++ src/sections/@dashboard/app/AppTasks.js
@@ -28,7 +28,7 @@ AppTasks.propTypes = {
 export default function AppTasks({ title, taskCompleted, subheader, list, ...other }) {
   const { control } = useForm({
     defaultValues: {
-      taskCompleted: ['2'],
+      taskCompleted,
     },
   });
 
