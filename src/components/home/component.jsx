50e4e3dc7f7f125a4bcd9d834cbbdc087857f3da
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 02:19:04 +0100

    Minor change

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -12,7 +12,7 @@ import {
 } from '../../sections/@dashboard/app';
 import { fetchDisasters, fetchEmissions } from "../../api/api";
 
-// const getUnique = (arr, comp) => [...new Set(arr.map(x => x[comp]))];
+const getUnique = (arr, comp) => [...new Set(arr.map(x => x[comp]))];
 
 const Home = (props) => {
 	const theme = useTheme();
