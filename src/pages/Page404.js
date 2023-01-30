9b9e4944992560c1818a3766e571eb47f58bcf23
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:54:38 +0100

    Minor update

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -37,7 +37,7 @@ export default function Page404() {
 
           <Box
             component="img"
-            src="/assets/illustrations/illustration_404.svg"
+            src={`https://raw.githubusercontent.com/mohammadzainabbas/global-warning/main/public/assets/illustrations/illustration_404.svg`}
             sx={{ height: 260, mx: 'auto', my: { xs: 5, sm: 10 } }}
           />
 
