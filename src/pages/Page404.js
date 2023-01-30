14f6af3c2692e72c7fc0880a2789daf2ad1f4588
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:52:33 +0100

    Improving code quality

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -22,7 +22,7 @@ export default function Page404() {
   return (
     <>
       <Helmet>
-        <title> 404 Page Not Found | Global Warning </title>
+        <title> {`404 Page Not Found | Global Warning`} </title>
       </Helmet>
 
       <Container>
