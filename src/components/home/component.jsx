241a45a8e764505065a4062785bbfa88d5665902
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:38:36 +0100

    Improving code readability

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -141,34 +141,6 @@ const Home = ({ data }) => {
             />
           </Grid>
 
-          <Grid item xs={12} md={6} lg={4}>
-            <AppTrafficBySite
-              title="Traffic by Site"
-              list={[
-                {
-                  name: 'FaceBook',
-                  value: 323234,
-                  icon: <Iconify icon={'eva:facebook-fill'} color="#1877F2" width={32} />,
-                },
-                {
-                  name: 'Google',
-                  value: 341212,
-                  icon: <Iconify icon={'eva:google-fill'} color="#DF3E30" width={32} />,
-                },
-                {
-                  name: 'Linkedin',
-                  value: 411213,
-                  icon: <Iconify icon={'eva:linkedin-fill'} color="#006097" width={32} />,
-                },
-                {
-                  name: 'Twitter',
-                  value: 443232,
-                  icon: <Iconify icon={'eva:twitter-fill'} color="#1C9CEA" width={32} />,
-                },
-              ]}
-            />
-          </Grid>
-
           <Grid item xs={12} md={6} lg={8}>
             <AppTasks
               title="Tasks"
