f841e8dde9c9e6c814d1ae66bdc1b57fadbc1876
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 05:56:00 +0100

    Fixing compatibility issues

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -185,8 +185,8 @@ const Home = (props) => {
 			data: sortBy(disaster_type.data, (r) => r.year),
 		}
 	});
-	const PICK_TOP = 5;
-	let top_disasters = slice(reverse(sortBy(disaster_type_count, (r) => r.data.length)), 0, PICK_TOP);
+	const PICK_TOP = 3;
+	let top_disasters = slice(reverse(sortBy(disaster_type_count, (r) => r.data.length)), 1, PICK_TOP);
 	top_disasters = top_disasters.map((disaster_type) => {
 		let result = {};
 		disaster_type.data.forEach(({ year, total_deaths }) => {
