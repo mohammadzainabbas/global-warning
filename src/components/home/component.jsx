2751d0ea93ec8c9fc1ba9eb62ef0f81e7474e71c
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 05:56:42 +0100

    Work in progress

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -14,7 +14,7 @@ import { fetchDisasters, fetchEmissions } from "../../api/api";
 import { styled } from '@mui/material/styles';
 import { sumBy, sortBy, reverse, min, max, slice, forEach } from 'lodash';
 
-const FILLS = ['gradient'];
+const FILLS = ['pattern'];
 // const FILLS = ['gradient', 'solid', 'pattern', 'image'];
 
 const getUnique = (arr, comp) => [...new Set(arr.map(x => x[comp]))];
