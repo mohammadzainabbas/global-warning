5888fa1d1dcc82492eb6391cdd7f322008a3a2c3
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:32:23 +0100

    New feature added

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Helmet } from 'react-helmet-async';
 import { useTheme } from '@mui/material/styles';
 import { Grid, Container, Typography } from '@mui/material';
 import {
