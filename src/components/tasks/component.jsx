2206fb6e317243047e80f94cf7417321cbce7006
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:41:23 +0100

    Adding missing documentation

--- src/components/tasks/component.jsx
+++ src/components/tasks/component.jsx
@@ -5,3 +5,31 @@ import { useTheme } from '@mui/material/styles';
 import { Grid, Container, Typography } from '@mui/material';
 
 import { AppTasks } from '../../sections/@dashboard/app';
+
+
+const Tasks = () => {
+
+    const theme = useTheme();
+    
+    return (
+        <React.Fragment>
+        <Helmet>
+            <title> Tasks | Global Warning </title>
+        </Helmet>
+    
+        <Container maxWidth="xl">
+            <Typography variant="h4" sx={{ mb: 5 }}>
+            {`Hi, Welcome back`}
+            </Typography>
+    
+            <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <AppTasks />
+            </Grid>
+            </Grid>
+        </Container>
+        </React.Fragment>
+    );
+};
+
+export default Tasks;
