e4e17afd1a0938b8bac5fa6e580b8da1c0d0a01a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 07:52:56 +0100

    Adding comments for better understanding

--- src/sections/@dashboard/app/AppCurrentSubject.js
+++ src/sections/@dashboard/app/AppCurrentSubject.js
@@ -45,13 +45,8 @@ export default function AppCurrentSubject({ title, subheader, chartData, chartCo
     stroke: { width: 2 },
     fill: { opacity: 0.48 },
     legend: { floating: true, horizontalAlign: 'center' },
-    yaxis: {
-      text: null,
-      labels: {
-        style: {
-          colors: chartColors,
-        },
-      },
+    y: {
+      
     },
     xaxis: {
       categories: chartLabels,
