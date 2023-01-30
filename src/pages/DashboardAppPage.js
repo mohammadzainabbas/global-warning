2022e45f6ff105568e42c28c503fa10d2d8adddb
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 01:09:11 +0100

    Fixing bugs

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -35,7 +35,7 @@ export default function DashboardAppPage() {
 
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6} md={3}>
-            <AppWidgetSummary title={`Total deaths`} total={714000} icon={'mdi:emoticon-dead'} />
+            <AppWidgetSummary title={`Total deaths`} total={714000} co icon={'mdi:emoticon-dead'} />
           </Grid>
 
           <Grid item xs={12} sm={6} md={3}>
