a3904c42bd3b4c5065126a275ab293c6d9b271b6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-30 05:25:03 +0100

    Fixing compatibility issues

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -197,7 +197,7 @@ const Home = (props) => {
 
 		debugger
 
-		result = Object.keys(result).forEach((year) => {
+		const data = Object.keys(result).forEach((year) => {
 			if (display_years.includes(parseInt(year))) {
 				return {
 					year: parseInt(year),
