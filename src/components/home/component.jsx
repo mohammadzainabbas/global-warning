ed3bfcd3b7fe12c265ac47a30677596cc5b0b7f1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 05:16:15 +0100

    Fixing compatibility issues

--- src/components/home/component.jsx
+++ src/components/home/component.jsx
@@ -121,13 +121,15 @@ const Home = (props) => {
 		setYearRange([_min_year, _max_year]);
 	};
 
+	// For disasters
 	const total_deaths = sumBy(disasters, "total_deaths") || 0;
 	const total_affected = sumBy(disasters, "total_affected") || 0;
 	const affected_countries = getUnique(disasters, "country")?.length || 0;
 	const total_disasters = disasters.length || 0;
+
+	// For emissions
 	const total_MTCO2e = Number(sumBy(emissions, "emission_value")) || 0;
 	const emissions_by_countries = getUnique(emissions, "country")?.length || 0;
-
 
 	return (
 		<React.Fragment>
