e4a6b8d31915435c2bf9733a87e4f3b01fcdde77
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 08:34:10 +0100

    Fixing linting errors

--- src/components/map/component.jsx
+++ src/components/map/component.jsx
@@ -12,16 +12,6 @@ import Map, { Source, Layer } from 'react-map-gl';
 import ControlPanel, { Mode } from './control-panel';
 
 const getUnique = (arr, comp) => [...new Set(arr.map(x => x[comp]))];
-const generateList = (a, b) => Array.from({ length: b - a + 1 }, (_, i) => a + i);
-const getRandomColor = () => {
-	const letters = '0123456789ABCDEF';
-	let color = '#';
-	for (let i = 0; i < 6; i++) {
-		color += letters[Math.floor(Math.random() * 16)];
-	}
-	return color;
-}
-
 function generateGradient(minColor, maxColor, numElements) {
 	let gradient = [];
 	let minRgb = hexToRgb(minColor);
