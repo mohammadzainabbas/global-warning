a00c665cfb855bb7000b0848b73a315f51fe33c2
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 05:52:35 +0100

    Adding missing sanitization

--- src/components/map/component.jsx
+++ src/components/map/component.jsx
@@ -187,7 +187,16 @@ const GlobalWarningMap = (props) => {
 								/>
 							</Grid>
 							<Grid item xs={12} sm={6} md={6}>
-
+								<Map
+									id="right-map"
+									{...viewState}
+									padding={rightMapPadding}
+									onMoveStart={onRightMoveStart}
+									onMove={activeMap === 'right' && onMove}
+									style={RightMapStyle}
+									mapStyle="mapbox://styles/mapbox/dark-v9"
+									mapboxAccessToken={TOKEN}
+								/>
 							</Grid>
 
 							<Grid item xs={12}>
