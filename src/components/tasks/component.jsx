9d9793b0be16052a7e053157c2598ff5c889b9e8
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 05:26:42 +0100

    Improving code maintainability

--- src/components/tasks/component.jsx
+++ src/components/tasks/component.jsx
@@ -11,33 +11,31 @@ import { AppTasks } from '../../sections/@dashboard/app';
 
 const Tasks = () => {
 
-    const theme = useTheme();
-    
     return (
         <React.Fragment>
             <Helmet>
                 <title> {`Tasks | Global Warning`} </title>
             </Helmet>
-        
+
             <Container maxWidth="xl">
                 <Typography variant="h4" sx={{ mb: 5 }}>
-                {`List of tasks to be completed`}
+                    {`List of tasks to be completed`}
                 </Typography>
-        
+
                 <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <AppTasks
-                        title="Tasks"
-                        list={[
-                        { id: '1', label: 'Create FireStone Logo' },
-                        { id: '2', label: 'Add SCSS and JS files if required' },
-                        { id: '3', label: 'Stakeholder Meeting' },
-                        { id: '4', label: 'Scoping & Estimations' },
-                        { id: '5', label: 'Sprint Showcase' },
-                        ]}
-                        taskCompleted={[`1`, `2`]}
-                    />
-                </Grid>
+                    <Grid item xs={12}>
+                        <AppTasks
+                            title="Tasks"
+                            list={[
+                                { id: '1', label: 'Create FireStone Logo' },
+                                { id: '2', label: 'Add SCSS and JS files if required' },
+                                { id: '3', label: 'Stakeholder Meeting' },
+                                { id: '4', label: 'Scoping & Estimations' },
+                                { id: '5', label: 'Sprint Showcase' },
+                            ]}
+                            taskCompleted={[`1`, `2`]}
+                        />
+                    </Grid>
                 </Grid>
             </Container>
         </React.Fragment>
