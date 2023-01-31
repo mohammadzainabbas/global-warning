51f9bd0b30cffa194e8b3541821f2a80f131c718
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 06:31:20 +0100

    Adding comments for better understanding

--- src/components/map/component.jsx
+++ src/components/map/component.jsx
@@ -170,7 +170,7 @@ const GlobalWarningMap = (props) => {
 			{loading ? <Loading /> :
 				<React.Fragment>
 					<Container maxWidth={`xl`}>
-						<Typography variant={`h4`} sx={{ mb: 5 }}>
+						<Typography variant={`subtitle2`} sx={{ mb: 5 }}>
 							{years.length ? `Showing stats from ${yearRange[0]} to ${yearRange[1]}` : `Hi, Welcome back`}
 						</Typography>
 
