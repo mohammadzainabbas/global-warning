f37003c3026ee3681ea8ce7d030cf95318fa8cba
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 05:28:07 +0100

    Improving code maintainability

--- src/components/tasks/component.jsx
+++ src/components/tasks/component.jsx
@@ -29,7 +29,7 @@ const Tasks = () => {
                             list={[
                                 { id: '1', label: 'Collect and clean datasets' },
                                 { id: '2', label: 'Perform exploratory data analysis (EDA)' },
-                                { id: '3', label: 'Stakeholder Meeting' },
+                                { id: '3', label: 'Develop and deploy webpage' },
                                 { id: '4', label: 'Scoping & Estimations' },
                                 { id: '5', label: 'Sprint Showcase' },
                             ]}
