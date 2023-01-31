5a1e78598b1ffb96988f6007c3fb560e90c302ea
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 04:38:02 +0100

    Updated code

--- src/components/emissions/component.jsx
+++ src/components/emissions/component.jsx
@@ -278,7 +278,7 @@ const Emissions = (props) => {
 
 							<Grid item xs={12} md={6} lg={4}>
 								<AppCurrentSubject
-									title={`Emissions by sector`}
+									title={`Emissions per sector`}
 									// chartLabels={disaster_type}
 									// chartData={disaster_type_wise}
 									chartLabels={[]}
