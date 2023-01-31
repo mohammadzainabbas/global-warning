5c581677a56cc72b1d051c10c0893212d7637850
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 01:43:26 +0100

    Minor

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -12,3 +12,8 @@ export const ACCOUNT = {
 	email: "mohammadzainabbas@gmail.com",
 	photoURL: "https://avatars.githubusercontent.com/u/19189095?v=4",
 };
+
+// MapGL access token
+export const MAPGL_TOKEN_PUBLIC = "pk.eyJ1IjoibW9oYW1tYWR6YWluYWJiYXMiLCJhIjoiY2xkamlhaGJ6MGI0dTNvbGdpaTExYjV1YSJ9.BiGXm90x-XxM6n-rHKEAVw";
+export const MAPGL_TOKEN = "sk.eyJ1IjoibW9oYW1tYWR6YWluYWJiYXMiLCJhIjoiY2xkamlmZXNhMDE4ODNxbWJ3eTc0a3VweCJ9.aeJcN-3ZhxIlkB_3KBm0BQ";
+export const MAPGL_TOKEN_NAME = "global-warning-visual-analytics";
