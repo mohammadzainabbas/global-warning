e8e97305694330d3fc2796b4ee14d72bc5275358
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 04:04:52 +0100

    Improving code quality

--- src/components/emissions/component.jsx
+++ src/components/emissions/component.jsx
@@ -212,7 +212,7 @@ const Emissions = (props) => {
 
 						<Grid container spacing={3}>
 							<Grid item xs={12} sm={6} md={3}>
-								<AppWidgetSummary title={`Total MTCO2e`} total={total_MTCO2e} color={`error`} icon={'mdi:emoticon-dead'} />
+								<AppWidgetSummary title={`Total GHG emissions (MtCO2e)`} total={total_MTCO2e} color={`error`} icon={'mdi:emoticon-dead'} />
 							</Grid>
 
 							<Grid item xs={12} sm={6} md={3}>
