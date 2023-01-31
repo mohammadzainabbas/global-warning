63bc7e426961646289348b0c3aee078f9b887ec1
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2023-01-31 05:35:29 +0100

    Improving code organization

--- src/components/tasks/component.jsx
+++ src/components/tasks/component.jsx
@@ -13,13 +13,13 @@ const generateList = (a, b) => Array.from({ length: b - a + 1 }, (_, i) => a + i
 const Tasks = () => {
 
     const tasks = [
-        { 'Collect and clean datasets' },
-        { 'Perform exploratory data analysis (EDA)' },
-        { 'Brainstorm different visualisation ideas' },
-        { 'Develop and deploy webpage for the project' },
-        { 'Presentation and Demo' },
-        { 'Public awareness' },
-        { 'Demo with interactive Maps' },
+        'Collect and clean datasets',
+        'Perform exploratory data analysis (EDA)',
+        'Brainstorm different visualisation ideas',
+        'Develop and deploy webpage for the project',
+        'Presentation and Demo',
+        'Public awareness',
+        'Demo with interactive Maps',
     ]
 
     return (
